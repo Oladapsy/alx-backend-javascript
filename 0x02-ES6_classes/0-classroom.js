dcc66d49c66a51674e6619c15265365c037eb867
oladapsy <dapoodedeyi@yahoo.com>
2024-10-13 22:34:30 +0100

    declaring_a_class_js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,14 @@
-export default class ClassRoom{
-  constructor(maxStudentsSize){
+/**
+ * Represent a class room
+ */
+export default class ClassRoom {
+  /**
+   * Create a new classroom
+   * @param {Number} maxStudentsSize - The maximum number of
+   *
+   * students in the class
+   */
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
