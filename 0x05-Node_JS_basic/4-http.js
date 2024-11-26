db01f5d52cb1a3aee73e93fcd1627c8926245af2
oladapsy <dapoodedeyi@yahoo.com>
2024-11-26 16:00:12 +0000

    mini server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const { createServer } = require('http');
+
+const port = 1245;
+const hostname = 'localhost';
+const app = createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  //write to the browser with res.write()
+  res.write('Hello Holberton School!');
+  
+  //end the response
+  res.end();
+});
+
+app.listen(port, hostname, () => {
+});
