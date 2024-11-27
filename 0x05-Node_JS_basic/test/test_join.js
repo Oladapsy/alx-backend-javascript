a0aa8d52c013c4127a9c9e2e946dff7cca2c046f
oladapsy <dapoodedeyi@yahoo.com>
2024-11-27 11:43:39 +0000

    express mini server

---
+++ 0x05-Node_JS_basic/test/test_join.js
@@ -0,0 +1,12 @@
+let array = [
+  "Number of students: 10",
+  "Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie",
+  "Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy"
+];
+
+
+
+const new_data = array.join('   ____    ');
+const new_data1 = array.join();
+console.log(new_data)
+console.log(new_data1)
